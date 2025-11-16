cc60338f193dacfa1ae611c179bdcf13178409de
josephmangara <josephmangara2@gmail.com>
2025-11-17 00:10:50 +0300

    Initialized components

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 // import { useState } from 'react'
+import Footer from "./navigation/Footer";
+import Header from "./navigation/Header";
 
 function App() {
   // const [count, setCount] = useState(0)
@@ -6,6 +8,8 @@ function App() {
   return (
     <>
       <Header />
+      <Home />
+      <Footer />
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router";
+import App from './App.jsx';
+import './index.css';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = document.getElementById("root");
+
+
+ReactDOM.createRoot(root).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
