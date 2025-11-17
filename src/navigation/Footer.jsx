a2b5d994e51d38146a89ef46bce1b0bb853045ac
josephmangara <josephmangara2@gmail.com>
2025-11-17 23:59:04 +0300

    Footer

--- src/navigation/Footer.jsx
+++ src/navigation/Footer.jsx
@@ -2,10 +2,11 @@
 
 function Footer() {
 
-return (
-    <>
-    </>
-)
+  return (
+    <footer className="w-full text-center py-4 border-t mt-10 text-sm">
+      © {new Date().getFullYear()} Journalist Portfolio. All rights reserved.
+    </footer>
+  );
 }
 
 export default Footer;
