284c09ce5a420329f06ced567ec1cbd7a469daec
josephmangara <josephmangara2@gmail.com>
2025-11-17 23:56:25 +0300

    stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 // import { useState } from 'react'
 import Footer from "./navigation/Footer";
 import Header from "./navigation/Header";
+import Home from "./body/Home";
 
 function App() {
   // const [count, setCount] = useState(0)
--- src/body/Home.jsx
+++ src/body/Home.jsx
@@ -3,10 +3,10 @@
 function Home() {
 
 return (
-    <>
+    <div>
       <h1 className="text-2xl p-8">Job Maina</h1>
       <p>Hi, xplore my portfolio.</p>
-    </>
+    </div>
 )
 }
 
